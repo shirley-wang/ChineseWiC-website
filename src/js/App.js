bf2bfc9b088e684ddbdeedb308fb3b86391e4cd7
shirley-wang <xuanli512@gmail.com>
2017-05-20 15:06:52 -0700

    first commit

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h2>Welcome to Chinese Women in Computing</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
