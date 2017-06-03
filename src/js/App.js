280ed1c23661032a10a35e2f1af1449c9f7e5b3c
shirley-wang <xuanli512@gmail.com>
2017-06-03 16:50:35 -0700

    adding scss style

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar.js';
+import './../css/style.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _bind(...methods) {
+    methods.forEach(method => this[method] = this[method].bind(this));
+  }
+
   render() {
     return (
-      <div className="App">
-          <h2>Welcome to Chinese Women in Computing</h2>
+      <div className="App container-fluid homepage">
+        <Navbar />
       </div>
     );
   }
---
+++ src/js/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <div className="">
+        <h1>tests</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
